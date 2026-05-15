f50ded936f51dd79f64d48a2ba03776a1dfe6e82
Sebaas <sebastiancasavilca14@gmail.com>
2026-05-14 23:18:17 -0500

    subiendo proyecto

---
+++ src/sistema/hospitalario/SistemaHospitalario.java
@@ -0,0 +1,10 @@
+package sistema.hospitalario;
+
+public class SistemaHospitalario {
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hola mundo");
+    }
+    
+}
